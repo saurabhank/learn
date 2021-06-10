aa64c833bfd58a376bca63670c39e743d4190dec
joye <yoursaurabh2705@gmail.com>
2021-06-11 02:12:09 +0530

    changed docker compose to kubernetes yaml

--- services/A/service_a.js
+++ services/A/service_a.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const axios = require('axios')
 var fs = require('fs');
+const pino = require('pino')
+const ecsFormat = require('@elastic/ecs-pino-format') // @elastic/ecs-pino-format
 
+const log = pino({ ...ecsFormat({ convertReqRes: true }) })
 
 // Constants
 const PORT = 3001;
@@ -12,6 +15,8 @@ const HOST = '0.0.0.0';
 const apm = require('elastic-apm-node').start({
   // Override service name from package.json
   // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  // logger: log,
+
   serviceName: 'service-a',
 
   // Use if APM Server requires a token
@@ -21,15 +26,19 @@ const apm = require('elastic-apm-node').start({
   apiKey: '',
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://apm:8200',
+  serverUrl: 'http://localhost:8200',
+  logLevel: 'trace'
 })
 
 count = 0
 // App
 const app = express();
 app.get('/', (req, res) => {
-  fs.writeFile('/usr/share/' + count + '.txt', 'Hello content!', function (err) {
+  fs.writeFile('/tmp/' + count + '.txt', 'Hello content!', function (err) {
     count += 1
+    traceIds = apm.currentTraceIds
+    apm.captureError({"hopefully": "reached"}, {params: "nothing seems right"})
+    apm.logger.error({"trace": "val"}, {msg: "hello lala" , "trace": traceIds})
     if (err) throw err;
     console.log('Saved!');
     res.send('Hello From Service A');
